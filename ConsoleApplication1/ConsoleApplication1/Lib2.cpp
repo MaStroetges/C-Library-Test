11554cdc280b183cc7f382eaa3b04fc9b65b80cf
MaStroetges <56864237+MaStroetges@users.noreply.github.com>
2021-09-07 23:02:07 +0200

    Update Lib2.cpp

--- ConsoleApplication1/ConsoleApplication1/Lib2.cpp
+++ ConsoleApplication1/ConsoleApplication1/Lib2.cpp
@@ -1,13 +1,13 @@
 #include "Lib2.h"
 
 
-// Methode mit gemeinsamen Strukturen:
+// Methode mit getrennten Strukturen:
 int Lib2::SummiereZahlen(DatenStruktur1_B daten)
 {
 	return daten.a + daten.b;
 }
 
-// Methode mit getrennten Strukturen:
+// Methode mit gemeinsamen Strukturen:
 int Lib2::SummiereZahlen2(DatenStruktur daten)
 {
 	return daten.a + daten.b;
